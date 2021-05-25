e6aa91c91fb32271a387b765b1474db776705b0e
Ozgur Yasar <ozguryasar2001@hotmail.com>
2021-05-26 02:24:29 +0300

    things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //create a new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
 
     }
 }
